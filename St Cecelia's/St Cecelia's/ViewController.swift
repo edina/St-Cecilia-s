2587c9755dc7ff670291a03fe6be006fdf07ad4e
Colin Gormley <colin.gormley@ed.ac.uk>
2016-05-20 10:42:32 +0100

    edina/St-Cecilia-s#1:

---
+++ St Cecelia's/St Cecelia's/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  St Cecelia's
+//
+//  Created by Colin Gormley on 20/05/2016.
+//  Copyright © 2016 Edina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
